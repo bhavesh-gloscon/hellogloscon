9f9d77aa83a5dd7069b9aa2fc50decc7afa3b16b
bhavesh <a-bpansheriya@gloscon.com>
2011-08-10 07:27:27 -0400

    First commit

--- lib/hellogloscon.rb
+++ lib/hellogloscon.rb
@@ -1,6 +1,6 @@
 
 module HelloGloscon
   def self.do
-    "Hello collegues from the all mighty hellogloscon Gem! This is developed by Bhavesh as Gem development practices"
+    "Hello GLOSCON SOLUTIONS from the hellogloscon Gem! This is developed by Bhavesh as Gem development practices"
   end
 end
