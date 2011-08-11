e4f97ccf517e3bfe4e6a3f1fc8e6f14fcff1c059
bhavesh <a-bpansheriya@gloscon.com>
2011-08-11 01:22:13 -0400

    gitk & giggle for git GUI

--- lib/hellogloscon.rb
+++ lib/hellogloscon.rb
@@ -1,6 +1,6 @@
 
 module HelloGloscon
   def self.do
-    "Hello GLOSCON SOLUTIONS from the hellogloscon Gem! This is developed by Bhavesh as Gem development practices"
+    "Hello GLOSCON SOLUTIONS from the hellogloscon Gem! This is developed by Bhavesh as Gem development practices \n gitk and giggle is GUI for git repository"
   end
 end
